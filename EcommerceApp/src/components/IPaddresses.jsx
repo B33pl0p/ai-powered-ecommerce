44646b066c9c8e3d49accd72ff22737898749e65
B33pl0p <biplopgiri120@protonmail.com>
2024-11-30 18:37:05 +0545

    Added global IP address variable

---
+++ EcommerceApp/src/components/IPaddresses.jsx
@@ -0,0 +1,10 @@
+// ipAddresses.js (or any file)
+
+const IP_ADDRESSES = {
+  PC_LOCAL : 'http://192.168.104.216',
+  DEVELOPMENT: '10.0.0.2',
+  PRODUCTION: 'https://api.production.com',
+  STAGING: 'https://api.staging.com'
+};
+
+export default IP_ADDRESSES;
