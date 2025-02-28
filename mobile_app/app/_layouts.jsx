1e2bafddb1b2252fee8c52034eeb3e310a733795
B33pl0p <biplopgiri120@protonmail.com>
2025-02-28 22:14:56 +0545

    react native paper provider added

--- mobile_app/app/_layouts.jsx
+++ mobile_app/app/_layouts.jsx
@@ -1,17 +1,18 @@
 import { Stack } from 'expo-router';
+import { Provider as PaperProvider } from 'react-native-paper'; // Import PaperProvider
 import ItemCards from './ItemCards';
 import ImagePickerModal from './ImagePickerModal';
 import ImageCropScreen from './ImageCropScreen';
+
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />      
-      <Stack.Screen name="ItemCards" component = {ItemCards} />
-      <Stack.Screen name="ImagePickerModal" component = {ImagePickerModal}/>
-      <Stack.Screen name="ImageCropScreen" component = {ImageCropScreen}/>
-
-
-      
-    </Stack>
+    <PaperProvider> {/* Wrap the whole stack inside PaperProvider */}
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />      
+        <Stack.Screen name="ItemCards" component={ItemCards} />
+        <Stack.Screen name="ImagePickerModal" component={ImagePickerModal} />
+        <Stack.Screen name="ImageCropScreen" component={ImageCropScreen} />
+      </Stack>
+    </PaperProvider>
   );
 }
